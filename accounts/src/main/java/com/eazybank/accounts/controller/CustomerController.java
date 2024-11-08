675b8e3881f90a01bddfed788bcc43fe175df61a
Sorin Popteanu <sorinpopteanu72@gmail.com>
2024-11-08 16:26:33 +0200

    Added observability and monitoring using Grafana, Loki, Alloy, Prometheus, Tempo

--- accounts/src/main/java/com/eazybank/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybank/accounts/controller/CustomerController.java
@@ -40,9 +40,9 @@ public class CustomerController {
             @RequestParam
             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
             String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        logger.debug("fetchCustomerDetails method start");
         CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
-
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.status(HttpStatus.OK)
                              .body(customerDetailsDto);
     }
